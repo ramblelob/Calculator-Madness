b984df2ef696988800aef88290c070f152760b29
ramblelob <aileen.ramelb@gmail.com>
2022-01-06 17:50:09 -0800

    Calculator main repository created, Darshini's calculator.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import DarshiniCalculator from './DarshiniCalculator/DarshiniCalculator.js';
+
+
+const App = () => {
+    
+    return (
+        <DarshiniCalculator />
+    )
+}
+
+export default App;
