193b23c538bd59ffce262a0f7619918ccc02824b
Darren Chang <73253544+Darrenyibochang@users.noreply.github.com>
2022-01-07 23:52:37 -0800

    Update Darren's Calculator

--- src/App.js
+++ src/App.js
@@ -1,61 +1,100 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./App.css";
+import DarrenCal from "./DarrenCal/cal";
 
 const App = () => {
-    const [result, setResult] = useState("");
+  const [result, setResult] = useState("");
 
-    const handleClick = event => {
-        setResult(result.concat(event.target.name));
+  const handleClick = (event) => {
+    setResult(result.concat(event.target.name));
+  };
+
+  const clear = () => {
+    setResult("");
+  };
+
+  const backspace = () => {
+    // setResult(result.slice(0, result.length - 1));
+    setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Err");
     }
+  };
 
-    const clear = () => {
-        setResult("");
-    }
+  return (
+    <>
+      <div>
+        <div className="container">
+          <form>
+            <input type="text" value={result} />
+          </form>
 
-    const backspace = () => {
-        // setResult(result.slice(0, result.length - 1));
-        setResult(result.slice(0, -1));
-    }
-
-    const calculate = () => {
-        try {
-            setResult(eval(result).toString());
-        } 
-        catch(err) {
-            setResult("Err")
-        }
-    }
-
-    return (
-        <div>
-            <div className="container">
-                <form>
-                    <input type="text" value={result}/>
-                </form>
-            
-            <div className="keypad">
-                <button id="clear" onClick={clear}>Clear</button>
-                <button id="backspace" onClick={backspace}>C</button>
-                <button name="/" onClick={handleClick}>&divide;</button>
-                <button name="7" onClick={handleClick}>7</button>
-                <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>9</button>
-                <button name="*" onClick={handleClick}>&times;</button>
-                <button name="4" onClick={handleClick}>4</button>
-                <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>6</button>
-                <button name="-" onClick={handleClick}>&ndash;</button>
-                <button name="1" onClick={handleClick}>1</button>
-                <button name="2" onClick={handleClick}>2</button>
-                <button name="3" onClick={handleClick}>3</button>
-                <button name="+" onClick={handleClick}>+</button>
-                <button name="0" onClick={handleClick}>0</button>
-                <button name="." onClick={handleClick}>.</button>
-                <button id="equals" onClick={calculate}>=</button>
-            </div>
-            </div>
+          <div className="keypad">
+            <button id="clear" onClick={clear}>
+              Clear
+            </button>
+            <button id="backspace" onClick={backspace}>
+              C
+            </button>
+            <button name="/" onClick={handleClick}>
+              &divide;
+            </button>
+            <button name="7" onClick={handleClick}>
+              7
+            </button>
+            <button name="8" onClick={handleClick}>
+              8
+            </button>
+            <button name="9" onClick={handleClick}>
+              9
+            </button>
+            <button name="*" onClick={handleClick}>
+              &times;
+            </button>
+            <button name="4" onClick={handleClick}>
+              4
+            </button>
+            <button name="5" onClick={handleClick}>
+              5
+            </button>
+            <button name="6" onClick={handleClick}>
+              6
+            </button>
+            <button name="-" onClick={handleClick}>
+              &ndash;
+            </button>
+            <button name="1" onClick={handleClick}>
+              1
+            </button>
+            <button name="2" onClick={handleClick}>
+              2
+            </button>
+            <button name="3" onClick={handleClick}>
+              3
+            </button>
+            <button name="+" onClick={handleClick}>
+              +
+            </button>
+            <button name="0" onClick={handleClick}>
+              0
+            </button>
+            <button name="." onClick={handleClick}>
+              .
+            </button>
+            <button id="equals" onClick={calculate}>
+              =
+            </button>
+          </div>
         </div>
-    );
-}
+      </div>
+      <DarrenCal />
+    </>
+  );
+};
 
 export default App;
